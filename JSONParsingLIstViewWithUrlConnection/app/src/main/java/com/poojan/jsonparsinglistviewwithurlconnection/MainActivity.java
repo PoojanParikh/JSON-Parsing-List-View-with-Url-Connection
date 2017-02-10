5cf9c1a8cb90abe3cb8559404a034873220e33c4
PoojanParikh <camkampar@gmail.com>
2017-02-10 11:40:53 +0530

    URL code is written

--- JSONParsingLIstViewWithUrlConnection/app/src/main/java/com/poojan/jsonparsinglistviewwithurlconnection/MainActivity.java
+++ JSONParsingLIstViewWithUrlConnection/app/src/main/java/com/poojan/jsonparsinglistviewwithurlconnection/MainActivity.java
@@ -3,11 +3,48 @@ package com.poojan.jsonparsinglistviewwithurlconnection;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static android.provider.ContactsContract.CommonDataKinds.Website.URL;
+
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            URL url = new URL("https://jsonplaceholder.typicode.com/posts");
+
+            try {
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+                InputStream stream = urlConnection.getInputStream();
+
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(stream));
+                StringBuffer stringBuffer = new StringBuffer();
+
+                while(bufferedReader.readLine()!=null){
+
+                    stringBuffer.append(bufferedReader.readLine());
+
+                }
+
+                String jsonString = bufferedReader.toString();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
     }
 }
