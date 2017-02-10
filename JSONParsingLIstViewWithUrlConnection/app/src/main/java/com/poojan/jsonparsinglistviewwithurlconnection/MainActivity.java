7c7ba7d50b13ab5e010858d2256205e9dc971cf2
PoojanParikh <camkampar@gmail.com>
2017-02-10 13:14:02 +0530

    JSON Parsing List view with Url Connection Completed

--- JSONParsingLIstViewWithUrlConnection/app/src/main/java/com/poojan/jsonparsinglistviewwithurlconnection/MainActivity.java
+++ JSONParsingLIstViewWithUrlConnection/app/src/main/java/com/poojan/jsonparsinglistviewwithurlconnection/MainActivity.java
@@ -1,7 +1,14 @@
 package com.poojan.jsonparsinglistviewwithurlconnection;
 
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -10,17 +17,32 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 
 import static android.provider.ContactsContract.CommonDataKinds.Website.URL;
 
 public class MainActivity extends AppCompatActivity {
 
+    int userId,id;
+    String title,body;
 
+    ListView listView;
+
+    CustomAdapter customAdapter;
+
+
+
+    ArrayList<Post> postArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
 
         try {
             URL url = new URL("https://jsonplaceholder.typicode.com/posts");
@@ -40,6 +62,40 @@ public class MainActivity extends AppCompatActivity {
                 }
 
                 String jsonString = bufferedReader.toString();
+
+                postArrayList = new ArrayList<>();
+
+                try {
+                    JSONArray rootArray = new JSONArray(jsonString);
+
+                    for (int i = 0; i <rootArray.length() ; i++) {
+                        Post post = new Post();
+                        JSONObject postObject = rootArray.getJSONObject(i);
+                        userId = postObject.getInt("userId");
+                        Log.d("tag", "parsingJson: "+userId);
+                        id = postObject.getInt("id");
+                        Log.d("tag", "parsingJson: "+id);
+                        title = postObject.getString("title");
+                        Log.d("tag", "parsingJson: "+title);
+                        body  = postObject.getString("body");
+                        Log.d("tag", "parsingJson: "+body);
+
+                        post.setUserId(userId);
+                        post.setId(id);
+                        post.setTitle(title);
+                        post.setBody(body);
+                        postArrayList.add(post);
+                    }
+
+                    customAdapter = new CustomAdapter(this,postArrayList);
+
+
+
+                    listView.setAdapter(customAdapter);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
---
+++ JSONParsingLIstViewWithUrlConnection/app/src/main/java/com/poojan/jsonparsinglistviewwithurlconnection/Post.java
@@ -0,0 +1,45 @@
+package com.poojan.jsonparsinglistviewwithurlconnection;
+
+/**
+ * Created by VNurtureTechnologies on 10/02/17.
+ */
+
+public class Post {
+
+    int userId,id;
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    String title,body;
+
+}
