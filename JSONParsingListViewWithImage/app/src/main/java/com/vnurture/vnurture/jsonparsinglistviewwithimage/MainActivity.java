1b76f96e01b9487a8766de6db2e53ec3a08f351f
PoojanParikh <camkampar@gmail.com>
2017-02-11 13:01:50 +0530

    Blank Commit for JSONParsingListViewWithImage

---
+++ JSONParsingListViewWithImage/app/src/main/java/com/vnurture/vnurture/jsonparsinglistviewwithimage/MainActivity.java
@@ -0,0 +1,13 @@
+package com.vnurture.vnurture.jsonparsinglistviewwithimage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
